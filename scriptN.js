e0d7d29b9c5f74c6c14af33ad442ee0a2282093b
soryanbenk-cloud <soryanbenk@gmail.com>
2026-06-17 14:07:23 +0200

    Update scriptN.js

--- scriptN.js
+++ scriptN.js
@@ -22,3 +22,124 @@ window.addEventListener("DOMContentLoaded", () => {
     isPlaying = false;
   });
 });
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  const video = document.getElementById("start");
+
+  if (!video) return;
+
+video.muted = true;
+video.playsInline = true;
+video.currentTime = 5;
+video.play();
+
+
+  video.addEventListener("ended", () => {
+     video.style.opacity = "0";
+	setTimeout(() => {
+            video.style.display = "none";
+        } ,300);
+  });
+});
+
+
+	document.addEventListener("DOMContentLoaded", () => {
+const button =document.getElementById("button");
+
+let count=0;
+const maxboxe=1;
+
+button.addEventListener("click", ()=>{
+	if(count >= maxboxe) {
+	document.getElementById("nono").remove();
+	setTimeout(() =>{
+
+	},1000);
+	count=0;
+	return;
+}
+	const rect=createRectangle();
+
+	
+	rect.style.height ="250px";
+	rect.style.backgroundColor ="black";
+	rect.style.width = "250px";
+	rect.style.top = "400px";
+	rect.style.left = "500px";
+	rect.style.border = "2px solid red";
+	rect.style.borderRadius = "1px" ;
+	rect.style.position = "absolute";
+	rect.style.color ="white";
+	rect.style.textAlign = "center";
+
+	rect.classList.add("glow");
+	
+
+	document.body.appendChild(rect);	
+	
+count++
+	
+});
+function createRectangle() {
+    const rect = document.createElement("div");
+rect.id = "nono";
+rect.classList.add("nono");
+rect.textContent=("j'ai acquérie des bases en informatiques dans mon lycée, de part le excel éffectuer, mais j'ai également améliorer ses compétences durant mon stage a l'entreprise kusmi tea ou j'ai utilisé notamment canva et enfin j'ai aussi crée ce site internet.\nSi j'ai choisi de mettre cette compétences en avant dans ce potrfolio c'est car je suis actuellement en 2nde Tne et souhaite faire une 1ere CIEL");
+return rect;
+	}
+});
+
+
+
+
+
+
+	document.addEventListener("DOMContentLoaded", () => {
+const button =document.getElementById("buttonxp");
+
+let count=0;
+const maxboxe=1;
+
+button.addEventListener("click", ()=>{
+	if(count >= maxboxe) {
+	document.getElementById("nonoX").remove();
+	setTimeout(() =>{
+
+	},1000);
+	count=0;
+	return;
+}
+	const rect=createRectangle();
+
+	
+	rect.style.height ="250px";
+	rect.style.backgroundColor ="black";
+	rect.style.width = "250px";
+	rect.style.top = "400px";
+	rect.style.left = "500px";
+	rect.style.border = "2px solid red";
+	rect.style.borderRadius = "1px" ;
+	rect.style.position = "absolute";
+	rect.style.color ="white";
+	rect.style.textAlign = "center";
+
+	rect.classList.add("glow");
+	
+
+	document.body.appendChild(rect);	
+	
+count++
+	
+});
+function createRectangle() {
+    const rect = document.createElement("div");
+rect.id = "nonoX";
+rect.classList.add("nonoX");
+rect.textContent=("j'ai éffecuer mon stage dans l'entreprises kusmi tea durant la période du 1er au 19 Décembre, durant se stae j'ai pu apprendre:\n La gestion d'un évenement (préparation),notamment lors de la collaboration avec meta. \n  j'ai aussi pu apprendre a utiliser avec canva en faisant des benchmark, et diaporama sur si youtube e ttwitch serait utile a l'entreprise.");
+return rect;
+	}
+});
+
+ 
